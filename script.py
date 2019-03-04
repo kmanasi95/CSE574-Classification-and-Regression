3c41261fe1bd6768ddf2fe73683c17be03efd672
Kundan Kumar <kk7272@rit.edu>
2019-03-04 15:21:17 -0500

    qda learn

--- script.py
+++ script.py
@@ -28,8 +28,20 @@ def qdaLearn(X,y):
     # Outputs
     # means - A d x k matrix containing learnt means for each of the k classes
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
-    
-    # IMPLEMENT THIS METHOD
+    data_map = {}
+    means = []
+    covmats = []
+    for i in range(X.shape[0]):
+        key = int(y[i])
+        try:
+            data_map[key].append(X[i])
+        except KeyError:
+            data_map[key] = [X[i]]      
+    for key, value in data_map.items():
+        data_map[key] = np.asarray(data_map[key])
+        means.append(np.mean(data_map[key], axis=0))
+        covmats.append(np.cov(data_map[key].T))
+    means = np.asarray(means).T 
     return means,covmats
 
 def ldaTest(means,covmat,Xtest,ytest):
@@ -133,9 +145,9 @@ else:
     X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'),encoding = 'latin1')
 
 # LDA
-means,covmat = ldaLearn(X,y)
-ldaacc,ldares = ldaTest(means,covmat,Xtest,ytest)
-print('LDA Accuracy = '+str(ldaacc))
+#means,covmat = ldaLearn(X,y)
+#ldaacc,ldares = ldaTest(means,covmat,Xtest,ytest)
+#print('LDA Accuracy = '+str(ldaacc))
 # QDA
 means,covmats = qdaLearn(X,y)
 qdaacc,qdares = qdaTest(means,covmats,Xtest,ytest)
@@ -163,8 +175,8 @@ zacc,zqdares = qdaTest(means,covmats,xx,np.zeros((xx.shape[0],1)))
 plt.contourf(x1,x2,zqdares.reshape((x1.shape[0],x2.shape[0])),alpha=0.3)
 plt.scatter(Xtest[:,0],Xtest[:,1],c=ytest)
 plt.title('QDA')
-
 plt.show()
+
 # Problem 2
 if sys.version_info.major == 2:
     X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))
@@ -202,8 +214,8 @@ plt.title('MSE for Train Data')
 plt.subplot(1, 2, 2)
 plt.plot(lambdas,mses3)
 plt.title('MSE for Test Data')
-
 plt.show()
+
 # Problem 4
 k = 101
 lambdas = np.linspace(0, 1, num=k)
